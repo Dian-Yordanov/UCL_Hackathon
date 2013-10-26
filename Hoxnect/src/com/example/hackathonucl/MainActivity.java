7a974474aca62a7f4bbdd62bdea9e7802098e95f
HoxXxi <hoxxxii@gmail.com>
2013-10-26 13:56:25 +0100

    Test Comment

--- Hoxnect/src/com/example/hackathonucl/MainActivity.java
+++ Hoxnect/src/com/example/hackathonucl/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	//Test1
+	//Test2
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
